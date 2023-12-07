8d0eda5ed75db6ab84576ae56ffac73a99214b75
0x0-14 <saigklkrishnan@gmail.com>
2023-12-08 03:12:03 +0530

    Inital Commit

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Initial Commit',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: const Color(0xff111118),
+        body: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              AnimatedTextKit(
+                isRepeatingAnimation: false,
+                animatedTexts: [
+                  TypewriterAnimatedText('Initial Commit',
+                      speed: const Duration(milliseconds: 200),
+                      textStyle: const TextStyle(
+                          fontSize: 100,
+                          color: Colors.white,
+                          fontFamily: "Source Code Pro")),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
